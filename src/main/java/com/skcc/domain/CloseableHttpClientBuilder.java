bdbcbd7e923dc87a46012355c75d941a916a06f7
aimmvp <aimmvp@gmail.com>
2024-02-14 13:49:47 +0900

    add setcookie

--- src/main/java/com/skcc/domain/CloseableHttpClientBuilder.java
+++ src/main/java/com/skcc/domain/CloseableHttpClientBuilder.java
@@ -3,8 +3,10 @@ package com.skcc.domain;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.cookie.BasicCookieStore;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.cookie.BasicClientCookie;
 import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
 import org.apache.hc.core5.http.URIScheme;
 import org.apache.hc.core5.http.config.Registry;
@@ -28,6 +30,13 @@ public class CloseableHttpClientBuilder {
      }
 
     public CloseableHttpClient build() {
+        final BasicCookieStore cookieStore = new BasicCookieStore();
+        final BasicClientCookie cookie = new BasicClientCookie("JSESSIONID", "1234");
+        cookie.setDomain(".sktelecom.com");
+        cookie.setAttribute("SM_USER", "P064065");
+        cookie.setAttribute("SM_COMPANY", "1000");
+        cookie.setAttribute("SMSESSION", "abcdef");
+        cookieStore.addCookie(cookie);
 
         return HttpClientBuilder.create().disableCookieManagement().useSystemProperties()
                 .setConnectionManager(new DomainHttpClientConnectionManager(getDomainRegistry(plainConnectionSocketFactory))).evictExpiredConnections()
@@ -38,6 +47,7 @@ public class CloseableHttpClientBuilder {
                                 .setResponseTimeout(
                                         Timeout.of(properties.getConnectionTimeout(), TimeUnit.MILLISECONDS))
                                 .build())
+                .setDefaultCookieStore(cookieStore)
                 .build();
     }
 
